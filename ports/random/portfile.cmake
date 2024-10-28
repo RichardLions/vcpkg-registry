249a1822660f02273a8f851cbaa0af13ffd42dd1
Richard Allen <Richard.Allen@ubisoft.com>
2024-10-28 17:04:08 +0000

    Updated port files

--- ports/random/portfile.cmake
+++ ports/random/portfile.cmake
@@ -5,13 +5,13 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO RichardLions/random
     REF "v0.1.0-testing"
-    SHA512 fa2c0c1c26124fe68f79565619bae6b2c6b5b11daa939a11a9e513fc38e1e5d817f30024dfd71cfb50fbdedcf1193f56ad2afd7ddc29299ac3ae335d9de53f54
+    SHA512 1099f1257531e5ddb8dbad12a2d8c66f5fdc8463106af87c0ec76cf62ee3c3a6afc7d82d1605501711ca5ae2e6cae4c3fa7221b14518ace2b3b06b5ee15c5e7c
     HEAD_REF master)
 
 vcpkg_install_msbuild(
     SOURCE_PATH "${SOURCE_PATH}"
     PROJECT_SUBPATH "random.sln"
-    ADDITIONAL_LIBS "random.lib")
+    USE_VCPKG_INTEGRATION)
 
 file(
     GLOB HEADER_FILES "${SOURCE_PATH}/random/*.h")
