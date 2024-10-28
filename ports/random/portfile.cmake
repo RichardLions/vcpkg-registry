eddadf0cd95daadeaa97ec8e8cb45e908edde993
Richard Allen <Richard.Allen@ubisoft.com>
2024-10-28 12:20:09 +0000

    Updated random vcpkg hash

--- ports/random/portfile.cmake
+++ ports/random/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO RichardLions/random
     REF "v0.1.0-testing"
-    SHA512 0
+    SHA512 fa2c0c1c26124fe68f79565619bae6b2c6b5b11daa939a11a9e513fc38e1e5d817f30024dfd71cfb50fbdedcf1193f56ad2afd7ddc29299ac3ae335d9de53f54
     HEAD_REF master
 )
 
