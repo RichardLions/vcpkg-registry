99b92600d7c9282cc386f1740c434c551e0fdf5f
Rich Allen <richard1.lions@gmail.com>
2025-01-19 18:32:08 +0000

    Updated port files

--- ports/fsm/portfile.cmake
+++ ports/fsm/portfile.cmake
@@ -1,19 +1,11 @@
 set(VCPKG_POLICY_SKIP_COPYRIGHT_CHECK enabled)
 
-vcpkg_check_linkage(
-    ONLY_STATIC_LIBRARY)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO RichardLions/state-pattern
     REF "v0.1.0"
     SHA512 93ce1b743092923fa70f400957ebaca83d2dea008add2c70f443b1175dd0f88339577aed5e520b05652ac9fc8f894ea0a4e8a3a455670f773b66c00694dc7df4
     HEAD_REF master)
-
-vcpkg_install_msbuild(
-    SOURCE_PATH "${SOURCE_PATH}"
-    PROJECT_SUBPATH "state-pattern.sln"
-    USE_VCPKG_INTEGRATION)
 
 file(
     GLOB HEADER_FILES "${SOURCE_PATH}/finitestatemachine.h")
