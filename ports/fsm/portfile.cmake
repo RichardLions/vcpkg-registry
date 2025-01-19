4e5b9e150a79691d0e3f234901739360f61ef66d
Rich Allen <richard1.lions@gmail.com>
2025-01-19 19:15:43 +0000

    Updated port files

--- ports/fsm/portfile.cmake
+++ ports/fsm/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO RichardLions/state-pattern
     REF "v0.1.0"
-    SHA512 0
+    SHA512 b2af2ace0ff3574aa6332ef039948239ba1dbefdc4f35b9ff68d5f999e95aadd3f6eb41abe382971ba30be9c76d645bbe4135d775c6179ed0723d595ef25c505
     HEAD_REF master)
 
 file(
