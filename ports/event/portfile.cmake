e9af57f4d20f3f21df1ae0c4f549a492e963a56e
Rich Allen <richard1.lions@gmail.com>
2025-01-25 17:18:37 +0000

    Updated port files

--- ports/event/portfile.cmake
+++ ports/event/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO RichardLions/event-pattern
     REF "v0.1.0"
-    SHA512 0
+    SHA512 6cec09325848e9a0e821a1cfbb502fac7eb43bffe95fa2f64c74b44a1f543a442c3ea4cec1126673ff9ee1c744968c54364fe4b37dc5a08c18959f33e225f9bb
     HEAD_REF master)
 
 file(
